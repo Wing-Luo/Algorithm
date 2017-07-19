128659670386943f0b378ef4c7a3fc0fc5b93512
wing Luo <Aquarius_wing@icloud.com>
2017-07-20 00:28:03 +0800

    初始化

--- 算法竞赛入门经典/第六章/example6-9.cpp
+++ 算法竞赛入门经典/第六章/example6-9.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool solve(int& W) {
 	int W1, D1, W2, D2;
-	bool b1 = true; b2 = true;
+	bool b1 = true, b2 = true;
 	cin >> W1 >> D1 >> W2 >> D2;
 	if (!W1) b1 = solve(W1);
 	if (!W2) b2 = solve(W2);
