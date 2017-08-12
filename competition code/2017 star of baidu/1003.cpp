0002fc3831fe7fa55cf99ab9d4b0f6208c90a277
aquarius_wing@icloud.com <aquarius_wing@icloud.com>
2017-08-12 13:18:20 +0800

    百度之星1003研究1

--- competition code/2017 star of baidu/1003.cpp
+++ competition code/2017 star of baidu/1003.cpp
@@ -1,4 +1,94 @@
 //
-// Created by qq785 on 2017/8/12.
+// Created by wing on 2017/8/12.
 //
 
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+
+#define mem(a,b) memset(a,b,sizeof(a))
+using namespace std;
+typedef long long LL;
+const int maxn=100000+5;
+const int maxm=1000+5;
+
+LL a[maxn],b[maxn];
+LL k[maxm],p[maxm];
+LL dp[maxm][15];//防御力为j,打出i点伤害以上时所需的最少晶石
+
+LL max(LL a,LL b)
+{
+    return a>b?a:b;
+}
+LL min(LL a,LL b)
+{
+    return a<b?a:b;
+}
+void insertSort_0(int arr[], int length)
+{
+    int i, j, key;
+    for (i = 0; i < length; i++){
+        key = arr[i];
+        for (j = i - 1; j >= 0; j--){
+            if (arr[j] > key) {
+                arr[j + 1] = arr[j];
+            }
+            else
+                break;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n, m;
+    while (scanf("%d%d", &n, &m) != EOF) {
+        LL upB=0/*最高防御*/,upP=0/*最高攻击*/,hp=0;/*最高生命*/
+        for(int i=0; i<n; i++)
+        {
+            scanf("%I64d%I64d",&a[i],&b[i]);
+            upB=max(upB,b[i]);
+            hp=max(hp,a[i]);
+        }
+
+        for(int i=0; i<m; i++)
+        {
+            scanf("%I64d%I64d",&k[i],&p[i]);
+            upP=max(upP,p[i]);
+        }
+        if(upB>=upP)
+        {
+            printf("-1\n");
+            continue;
+        }
+        mem(dp,0);
+        for(int i=0; i<=10; i++)//防御
+        {
+            for(int j=1;j<=hp;j++)//造成伤害值
+            {
+                dp[j][i]=1e18;
+                for(int u=0;u<m;u++)//第u个技能
+                {
+                    LL dmg=p[u]-i;//第u个技能能造成的伤害
+                    if(dmg<=0)
+                        continue;
+                    if(dmg>=j)
+                    {
+                        dp[j][i]=min(dp[j][i],k[u]);
+                    }
+                    else
+                    {
+                        dp[j][i]=min(dp[j][i],dp[j-dmg][i]+k[u]);
+                    }
+                }
+            }
+        }
+        LL ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=dp[a[i]][b[i]];
+        }
+        printf("%I64d\n",ans);
+    }
+}
