ab06b82a94169596df11a6c0f4a5b6f2cc207a2d
aquarius_wing@icloud.com <aquarius_wing@icloud.com>
2017-08-13 23:27:36 +0800

    百度之星1003研究2(更新了一击必杀的函数算法)

--- competition code/2017 star of baidu/1003.cpp
+++ competition code/2017 star of baidu/1003.cpp
@@ -16,7 +16,8 @@ const int maxm=1000+5;
 LL a[maxn],b[maxn];
 LL k[maxm],p[maxm];
 LL dp[maxm][15];//防御力为j,打出i点伤害以上时所需的最少晶石
-
+LL isDped[maxm][15];
+int n,m;
 LL max(LL a,LL b)
 {
     return a>b?a:b;
@@ -40,21 +41,56 @@ void insertSort_0(int arr[], int length)
         arr[j + 1] = key;
     }
 }
+LL  getDp(LL hp,LL defense){
+    printf("hp:%lld,defense:%lld\n",hp,defense);
+    if (isDped[hp][defense] == 0) { // 如果为0表示需要计算
+        for(int u=0;u<m;u++)//第u个技能
+        {
+            LL dmg=p[u]-defense;//第u个技能能造成的伤害
+            if(dmg<=0)
+                continue;
+            if(dmg>=hp)
+            {
+                if(isDped[hp][defense] == 0){
+                    dp[hp][defense]=k[u];
+                    isDped[hp][defense] = 1;
+                }else{
+                    dp[hp][defense]=min(getDp(hp,defense),k[u]);
+                }
+                if(u == m -1){
+                    return dp[hp][defense];
+                }
+            }
+            else
+            {
+                dp[hp][defense]=min(getDp(hp,defense),getDp(hp-dmg,defense)+k[u]);
+                if(u == m -1){
+                    return dp[hp][defense];
+                }
+            }
+        }
+    }else{
+        return dp[hp][defense];
+    }
+
+}
 
 int main() {
-    int n, m;
     while (scanf("%d%d", &n, &m) != EOF) {
+        printf("n:%d,m:%d\n",n,m);
         LL upB=0/*最高防御*/,upP=0/*最高攻击*/,hp=0;/*最高生命*/
         for(int i=0; i<n; i++)
         {
-            scanf("%I64d%I64d",&a[i],&b[i]);
+            scanf("%lld%lld",&a[i],&b[i]);
+            printf("a:%lld,b:%lld\n",a[i],b[i]);
             upB=max(upB,b[i]);
             hp=max(hp,a[i]);
         }
 
         for(int i=0; i<m; i++)
         {
-            scanf("%I64d%I64d",&k[i],&p[i]);
+            scanf("%lld%lld",&k[i],&p[i]);
+            printf("k:%lld,p:%lld\n",k[i],p[i]);
             upP=max(upP,p[i]);
         }
         if(upB>=upP)
@@ -63,7 +99,8 @@ int main() {
             continue;
         }
         mem(dp,0);
-        for(int i=0; i<=10; i++)//防御
+        mem(isDped,0);
+        /*for(int i=0; i<=10; i++)//防御
         {
             for(int j=1;j<=hp;j++)//造成伤害值
             {
@@ -83,12 +120,16 @@ int main() {
                     }
                 }
             }
+        }*/
+        for(int i=0;i<n;i++)
+        {
+            getDp(a[i],b[i]);
         }
         LL ans=0;
         for(int i=0;i<n;i++)
         {
             ans+=dp[a[i]][b[i]];
         }
-        printf("%I64d\n",ans);
+        printf("%lld\n",ans);
     }
 }
