3e48e8925ec575f1b316a6e6099d11543239c3f8
aquarius_wing@icloud.com <aquarius_wing@icloud.com>
2017-08-17 07:27:54 +0800

    poj3219 改进失败

--- competition code/poj/3219.cpp
+++ competition code/poj/3219.cpp
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <cmath>
-const int maxn =(int) pow(2, 31);
+const int maxn = 2 << 31 + 1;
 int n,k;
 int array[maxn+1][maxn+1];
 int getBinomialCoefficients(int n, int k){
@@ -16,9 +16,22 @@ int getBinomialCoefficients(int n, int k){
     if(n > 0 && (k == 0 || n == k)){
         return 1;
     }
-    if(k < n){
-        return getBinomialCoefficients(n-1,k-1)+getBinomialCoefficients(n-1,k);
+    if(k>n/2){
+        k = n-k+1;
     }
+    // k是在n的左半边
+    for(int i_n = 0;i_n <= n;i_n++){
+        for(int i_k = 0; i_k <= k && i_k<=i_n ; i_k++){
+            if(i_k == 0 || i_k == i_n){
+                array[i_n][i_k] = 1;
+            }else{
+                array[i_n][i_k] = array[i_n-1][i_k-1] + array[i_n-1][i_k];
+                printf("array[%d][%d]:%d,array[%d][%d]:%d,array:%d\n",i_n-1,i_k-1,array[i_n-1][i_k-1],i_n-1, i_k , array[i_n-1][i_k], array[i_n][i_k]);
+            }
+            printf("i_n:%d,i_k:%d,array:%d\n",i_n, i_k, array[i_n][i_k]);
+        }
+    }
+    return array[n][k];
 }
 int main() {
     while (scanf("%d%d", &n, &k) != EOF) {
