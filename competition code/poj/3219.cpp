e4358215d615f1c334e81f7b954bf9391ec50261
aquarius_wing <aquarius_wing@icloud.com>
2017-08-17 18:13:23 +0800

    poj3219(2)

--- competition code/poj/3219.cpp
+++ competition code/poj/3219.cpp
@@ -8,34 +8,58 @@
 #include <algorithm>
 #include <cstdio>
 #include <cmath>
-const int maxn = 2 << 31 + 1;
+typedef long long ll ;
 int n,k;
-int array[maxn+1][maxn+1];
-int getBinomialCoefficients(int n, int k){
-    printf("n:%d,k:%d\n",n, k);
-    if(n > 0 && (k == 0 || n == k)){
+// [from,to] 连乘
+int getLucas(int n, int m, int p){
+    //printf("n:%d,m:%d,p:%d\n",n, m, p);
+    if(m == 0 || n == m){
         return 1;
     }
-    if(k>n/2){
-        k = n-k+1;
+    if(n < m || n%p < m%p){
+        return 0;
     }
-    // k是在n的左半边
-    for(int i_n = 0;i_n <= n;i_n++){
-        for(int i_k = 0; i_k <= k && i_k<=i_n ; i_k++){
-            if(i_k == 0 || i_k == i_n){
-                array[i_n][i_k] = 1;
-            }else{
-                array[i_n][i_k] = array[i_n-1][i_k-1] + array[i_n-1][i_k];
-                printf("array[%d][%d]:%d,array[%d][%d]:%d,array:%d\n",i_n-1,i_k-1,array[i_n-1][i_k-1],i_n-1, i_k , array[i_n-1][i_k], array[i_n][i_k]);
-            }
-            printf("i_n:%d,i_k:%d,array:%d\n",i_n, i_k, array[i_n][i_k]);
-        }
+    if(m>n/2){
+        m = n-m+1;
     }
-    return array[n][k];
+    return getLucas(n/p,m/p,p)*getLucas(n%p,m%p,p) % p;
 }
+
+ll f[2];
+void init(int p)
+{       //f[n] = n!
+    f[0] = 1;
+    for (int i=1; i<=p; ++i) f[i] = f[i-1] * i % p;
+}
+ll pow_mod(ll a, ll x, int p)
+{
+    ll ret = 1;
+    while (x)
+    {
+        if (x & 1)  ret = ret * a % p;
+        a = a * a % p;
+        x >>= 1;
+    }
+    return ret;
+}
+
+ll Lucas(ll n, ll k, int p)        //C (n, k) % p
+{
+    ll ret = 1;
+    while (n && k)
+    {
+        ll nn = n % p, kk = k % p;
+        if (nn < kk) return 0;  //inv (f[kk]) = f[kk] ^ (p - 2) % p
+        ret = ret * f[nn] * pow_mod (f[kk] * f[nn-kk] % p, p - 2, p) % p;
+        n /= p, k /= p;
+    }
+    return ret;
+}
+// 1073741824 573741803
 int main() {
+    //init (2);
     while (scanf("%d%d", &n, &k) != EOF) {
-        printf("%d\n",getBinomialCoefficients(n, k));
+        printf("%d\n",getLucas(n, k, 2));
     }
     return 1;
 }
